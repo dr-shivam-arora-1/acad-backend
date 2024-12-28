9716706af497cd91b7693bdae6d024c221ce98b5
Shivam Arora <shivamarora2002@gmail.com>
2024-12-28 16:25:52 +0530

    added date and time

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from datetime import datetime
+
 app=FastAPI(docs_url="/docs",title="Acad Backend")
 
 @app.get("/heartbeat")
 def heartbeat():
-    return "Application is working"
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+    return f"Application is running at {current_time}"

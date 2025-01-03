57f982a5824e3423df469bf645fb4e48b9305816
Shivam Arora <shivamarora2002@gmail.com>
2025-01-03 20:22:26 +0530

    task-6

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from datetime import datetime
+from app.router.resource import router
+from app.config.constant import SWAGGER_TAGS
 
-app=FastAPI(docs_url="/docs",title="Acad Backend")
+app = FastAPI(docs_url="/docs", title="Acad Backend", openapi_tags=SWAGGER_TAGS)
+app.include_router(router, prefix="/resource")
 
 @app.get("/heartbeat")
 def heartbeat():
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-
     return f"Application is running at {current_time}"
